a78e04ac2415bb23f2cb68891a3721d807534226
Zhou Hao <zhouhao.zhou@ncs.com.sg>
2022-11-09 18:50:27 +0800

    Initial Commit

---
+++ XcodeCloudDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XcodeCloudDemo
+//
+//  Created by Zhou Hao on 9/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
